ce69714cc63af22fbd8f06ce6f56266cad569b98
Connor Goudie <connorgoudie@live.com>
2017-03-18 16:49:56 -0700

    Draws textbox and words on button click

--- scripts/script.js
+++ scripts/script.js
@@ -6,13 +6,17 @@ function addText() {
   var text = document.getElementById("inputArea").value;
   var lineLength = text.length;
   ctx.font="20px Courier";
-  ctx.fillText(text + lineLength,10,currLine += 30);
-  ctx.
-
+  ctx.fillText(text, 15, currLine += 30);
+  
+  ctx.beginPath();
+  ctx.rect(10, currLine - 17, lineLength * 13, 20);
+  ctx.stroke();
 }
 
-function textBox() extends Drawable{
-
+var textBox = new Drawable(){
+  this.drawText = new addText() {
+    
+  }
 }
 
 function Drawable() {
