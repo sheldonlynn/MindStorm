cc0537d8a1edf50d824592f3741167bcba377b0b
Tyler Da Costa <dacosta-t@hotmail.com>
2017-03-19 07:37:58 -0700

    Loads screen on late login

--- scripts/script.js
+++ scripts/script.js
@@ -21,6 +21,16 @@ var wrapper = {
 
 var socket = io();
 
+socket.on('update screen', function(boxes) {
+  for(var i = 0; i < boxes.length; i++) {
+    box = boxes[i];
+    if(box.id != 'removed') {
+      drawBox(box.id, box.x, box.y, box.text);
+      boxArray.push(box);
+    }
+  }
+});
+
 board.addEventListener('click', createBox, false);
 
 function createBox(e) {
@@ -52,7 +62,7 @@ function deleteThisBox(e) {
 function deleteBox(currBoxId) {
   for (var i = 0; i < boxArray.length; i++){
     if (boxArray[i].id == currBoxId){
-      boxArray[i].id = "removed";
+      boxArray[i].id = 'removed';
     }
   }
   document.getElementById(currBoxId).remove();
@@ -118,8 +128,8 @@ function divMove(e) {
 }
 
 
-var seconds = 0;
-var minutes = 5;
+var seconds = 2;
+var minutes = 0;
 var watch = document.getElementById('h1');
 var start = document.getElementById('start');
 var timerStarted;
@@ -155,11 +165,63 @@ function countDown() {
 }
 
 start.onclick = function() {
-  if (!timerStarted)
-    timer();
+  if (!timerStarted) {
+    var timer = setInterval(function() {
+      seconds--;
+      if (seconds < 0 && minutes > 0) {
+        seconds = 59;
+        minutes--;
+      }
+      if (seconds < 10) {
+        document.getElementById('clock').innerHTML = minutes + ':0' + seconds;
+      } else {
+        document.getElementById('clock').innerHTML = minutes + ':' + seconds;
+      }
+
+      if (seconds == 0 && minutes == 0) {
+        clearInterval(timer);
+        seconds = 10;
+        minutes = 0;
+        timerStarted = false;
+        document.getElementById('clock').innerHTML = "Time's up";
+
+        var textareas = document.getElementsByTagName('textarea');
+
+        for (var i = 0; i < textareas.length; i++) {
+          textareas[i].readOnly = true;
+        }
+        return;
+      }
+    }, 1000);
+  }
 }
 
 function timer() {
   timerStarted = true;
-  setTimeout(countDown, 1000);
+  setInterval(function() {
+    seconds--;
+    if (seconds < 0 && minutes > 0) {
+      seconds = 59;
+      minutes--;
+    }
+    if (seconds < 10) {
+      document.getElementById('clock').innerHTML = minutes + ':0' + seconds;
+    } else {
+      document.getElementById('clock').innerHTML = minutes + ':' + seconds;
+    }
+
+    if (seconds == 0 && minutes == 0) {
+      seconds = 10;
+      minutes = 0;
+      timerStarted = false;
+      document.getElementById('clock').innerHTML = "Time's up";
+
+      var textareas = document.getElementsByTagName('textarea');
+
+      for (var i = 0; i < textareas.length; i++) {
+        textareas[i].readOnly = true;
+      }
+      return;
+    }
+  }, 1000);
 }
