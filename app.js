5fe8277e25072879c9b20ee5534f257b9913dd68
Tyler Da Costa <dacosta-t@hotmail.com>
2017-03-19 03:35:44 -0700

    Retrieve message from note

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+app.use('/', express.static(__dirname + '/'));
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+var fs = require('fs');
+var lineReader = require('readline');
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('send message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('send message', msg);
+  });
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
