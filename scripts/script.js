2281537713f1d89d857970b460160884278ab294
Connor Goudie <connorgoudie@live.com>
2017-03-18 21:06:54 -0700

    Fixed box duplication

--- scripts/script.js
+++ scripts/script.js
@@ -2,37 +2,45 @@ var board = document.getElementById('board');
 var boxIndex = 0;
 var xPos = 0;
 var yPos = 0;
+var currBox;
+var mouseHold = false;
 
 board.addEventListener('click', createBox, false);
 
 function createBox(e) {
-  var box = document.createElement('div');
+  if (!mouseHold) {
+    var box = document.createElement('div');
 
-  box.style.top = e.pageY;
-  box.style.left = e.pageX;
+    box.style.top = e.pageY;
+    box.style.left = e.pageX;
 
-  box.id = "box" + boxIndex++;
-  box.className = "box";
+    box.id = "box" + boxIndex++;
+    box.className = "box";
 
-  box.addEventListener('mousedown', mouseDown, false);
-  box.addEventListener('mouseup', mouseUp, false);
+    box.addEventListener('mousedown', mouseDown, false);
+    box.addEventListener('mouseup', mouseUp, false);
 
-  board.appendChild(box);
+    console.log("ihvaisd");
+    board.appendChild(box);
+  }
+  mouseHold = false;
 }
 
 function mouseUp(e) {
-  e.target.removeEventListener('mousemove', divMove, true);
+  currBox.removeEventListener('mousemove', divMove, true);
 }
 
 function mouseDown(e) {
-  var currBox = document.getElementById(e.target.id);
+  mouseHold = true;
+  currBox = e.target;
   xPos = e.clientX - currBox.offsetLeft;
   yPos = e.clientY - currBox.offsetTop;
   currBox.addEventListener('mousemove', divMove, true);
+  console.log("ayy");
 }
 
 function divMove(e) {
-  var currBox = e.target;
   currBox.style.top = (e.clientY - yPos)  + 'px';
   currBox.style.left = (e.clientX - xPos) + 'px';
+  console.log("shit broke yo");
 }
