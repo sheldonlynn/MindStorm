34fba41c2c6c65f1ba7815429d5f7658bd3bf994
Sheldon Lynn <sheldonlynn@outlook.com>
2017-03-18 18:27:22 -0700

    Implemented creation of div boxes on mouse click

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,17 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-var currLine = 10;
+var board = document.getElementById('board');
 
-function addText() {
-  var text = document.getElementById("inputArea").value;
-  var lineLength = text.length;
-  ctx.font="20px Courier";
-  ctx.fillText(text, 15, currLine += 30);
-  
-  ctx.beginPath();
-  ctx.rect(10, currLine - 17, lineLength * 13, 20);
-  ctx.stroke();
-}
+board.addEventListener('click', getClickPosition, false);
 
-var textBox = new Drawable(){
-  this.drawText = new addText() {
-    
-  }
-}
+function getClickPosition(el) {
+  var xPosition = el.pageX;
+  var yPosition = el.pageY;
 
-function Drawable() {
-  this.init = function(x, y, width, height) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-  }
+  console.log(xPosition, yPosition);
+
+  var box = document.createElement('div');
+  box.className = "box";
+  box.style.top = yPosition;
+  box.style.left = xPosition;
+
+  board.appendChild(box);
 }
